b9d19dddfec14ddac0d80af554e389bbbb8da429
nima <nima@home>
2024-03-09 19:58:57 +0545

    form fix

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(
-    session({
-        secret: process.env.URL,
-        saveUninitialized: true,
-        resave: false,
-        cookie: {
-            maxAge: 60 * 60 * 1000,
-        },
-    })
+  session({
+    secret: process.env.URL,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+  })
 );
 
 require("./config/passportConfig");
@@ -47,18 +47,18 @@ app.use("/dashboard", dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
